3c76ab5511571840f83f821f62635ce362a23c4f
Kyle Nakamura <kjnakamura@apu.edu>
2018-12-18 22:42:43 -0800

    Initialize Xcode project, CocoaPods, gitignore.

---
+++ Alarm Clock Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alarm Clock Manager
+//
+//  Created by Kyle Nakamura on 12/18/18.
+//  Copyright © 2018 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-Alarm Clock Manager/Pods-Alarm Clock Manager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Alarm_Clock_Manager : NSObject
+@end
+@implementation PodsDummy_Pods_Alarm_Clock_Manager
+@end
